6b910de688a41c48d862b9cea4d740cde77aac1b
TomaszP17 <tplucinski15@gmail.com>
2024-09-18 11:13:27 +0200

    my changes

--- jobportal-backend/jobportal/src/main/java/com/jobportal/jobportal/controllers/PaymentController.java
+++ jobportal-backend/jobportal/src/main/java/com/jobportal/jobportal/controllers/PaymentController.java
@@ -1,18 +1,14 @@
 package com.jobportal.jobportal.controllers;
 
-import com.jobportal.jobportal.dtos.payment.CheckoutRequestDTO;
+
 import com.jobportal.jobportal.dtos.payment.CreatePaymentRequestDTO;
 import com.jobportal.jobportal.services.payment.PaymentService;
 import com.stripe.exception.StripeException;
-import com.stripe.model.checkout.Session;
-import com.stripe.param.checkout.SessionCreateParams;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
